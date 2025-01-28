30bc14cc15e39fa5beb81bd107a20f455862e3f3
SteliosAlegkakis <stelios.alegak@gmail.com>
2025-01-28 21:32:02 +0200

    ui for index-fields page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,44 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ScrollView, SafeAreaView , Text, Alert } from 'react-native';
+import FieldPreview from '@/components/FieldPreview';
+import RoundButton from '@/components/RoundButton';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+const fields: any[] = [
+  { name: 'Χωράφι 1', fieldId: 0 },
+  { name: 'Χωράφι 2', fieldId: 1 },
+  { name: 'Χωράφι 3', fieldId: 2 },
+  { name: 'Χωράφι 4', fieldId: 3 },
+  { name: 'Χωράφι 5', fieldId: 4 },
+  { name: 'Χωράφι 6', fieldId: 5 },
+  { name: 'Χωράφι 7', fieldId: 6 },
+  { name: 'Χωράφι 8', fieldId: 7 },
+];
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        {fields.length === 0 ? (
+          <Text style={styles.title}>Δεν υπάρχουν χωράφια</Text>
+        ) : (
+          fields.map((field, index) => (
+            <FieldPreview key={index} name={field.name} field_id={field.fieldId} />
+          ))
+        )}
+      </ScrollView>
+      <RoundButton onPress={() => Alert.alert('Button Pressed!')} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   title: {
+    color: 'white',
+    textAlign: 'center',
+    marginTop: '50%',
     fontSize: 20,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  container: {
+    flex: 1,
   },
 });
