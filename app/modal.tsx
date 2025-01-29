3d36d6c8cfa1e89b706fe386a1d3109e879befd6
SteliosAlegkakis <stelios.alegak@gmail.com>
2025-01-29 15:35:54 +0200

    added settings page ui

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,18 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { StyleSheet } from 'react-native';
+import { View } from '@/components/Themed';
+import Setting from '@/components/Setting';
 
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/modal.tsx" />
-
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+      <Setting name="Ελληνικά" icon="language" />
+      <Setting name="Εξαγωγη Δεδομένων" icon="picture-as-pdf" />
+      <Setting name="Αποσύνδεση" icon="exit-to-app" />
     </View>
   );
 }
@@ -20,8 +15,6 @@ export default function ModalScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
     fontSize: 20,
