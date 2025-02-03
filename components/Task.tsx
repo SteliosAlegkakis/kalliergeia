f27c7c7d258e143b61669fa9a1b514f5f04ccf86
SteliosAlegkakis <stelios.alegak@gmail.com>
2025-02-03 13:35:29 +0200

    added Task component

---
+++ components/Task.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+
+interface TaskProps {
+    type: string;
+    date: string;
+    task_id: number;
+}
+
+const Task: React.FC<TaskProps> = ({ type, date, task_id }) => {
+    return (
+        <TouchableOpacity style={styles.container} onPress={() => {}}>
+            <Text style={styles.title}>{type}</Text>
+            <Text style={styles.title}>{date}</Text>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        width: "99%",
+        backgroundColor: '#282828',
+        padding: 20,
+        marginTop: 10,
+        marginBottom: 10,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    title: {
+        fontSize: 20,
+        paddingLeft: 10,
+        color: '#fff',
+        fontWeight: 'bold',
+    },
+});
+
+
+export default Task;
