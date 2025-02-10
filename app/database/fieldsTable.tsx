3f7dc511409b3f30b0f5ff484434b5f3a298673b
SteliosAlegkakis <stelios.alegak@gmail.com>
2025-02-10 15:33:21 +0200

    added get update and delete functions

--- app/database/fieldsTable.tsx
+++ app/database/fieldsTable.tsx
@@ -13,3 +13,27 @@ export const addField = (name: string, location: string, totalTrees: number, siz
         `
     );
 }
+
+export const getField = async (id: number) => {
+    const field:any = await db.execAsync(`SELECT * FROM fields WHERE id = ${id}`);
+    return field;
+}
+
+export const updateField = (id: number, name: string, location: string, totalTrees: number, size: number, description: string) => {
+    db.execAsync(
+        `
+        UPDATE fields
+        SET name = '${name}', location = '${location}', total_trees = ${totalTrees}, size = ${size}, description = '${description}'
+        WHERE id = ${id};
+        `
+    );
+}
+
+export const deleteField = (id: number) => {
+    db.execAsync(
+        `
+        DELETE FROM fields
+        WHERE id = ${id};
+        `
+    );
+}
