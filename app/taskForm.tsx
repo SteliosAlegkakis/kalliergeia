9fd1263f0bc34eb74d45d537a2631aae53ff9cbc
SteliosAlegkakis <stelios.alegak@gmail.com>
2025-02-12 15:22:44 +0200

    added date picker

--- app/taskForm.tsx
+++ app/taskForm.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Button, StyleSheet, Alert, Pressable, ScrollView } from 'react-native';
 import { useRouter, useNavigation } from 'expo-router';
 import DropDownPicker from 'react-native-dropdown-picker';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function FormScreen() {
 
@@ -9,8 +10,6 @@ export default function FormScreen() {
   const navigation = useNavigation();
 
   const [description, setDescription] = useState('');
-  const date = new Date();
-  const formattedDate = date.toLocaleDateString('el-GR'); // (e.g. 20/2/2022)
 
   const [oilQuantity, setOilQuantity] = useState<any>();
   const [oliveQuantity, setOliveQuantity] = useState<any>();
@@ -26,12 +25,25 @@ export default function FormScreen() {
   const [open, setOpen] = useState(false);
   const [taskType, setTaskType] = useState<string | null>(null);
   const [items, setItems] = useState([
-    { label: 'Άλεση', value: 'Άλεση' },
-    { label: 'Λίπασμα', value: 'Λίπασμα' },
-    { label: 'Πότισμα', value: 'Πότισμα' },
-    { label: 'Συγκομιδή', value: 'Συγκομιδή' },
-    { label: 'Ψέκασμα', value: 'Ψέκασμα' },
+    { label: 'Άλεση', value: 'grinding' },
+    { label: 'Λίπασμα', value: 'fertilization' },
+    { label: 'Πότισμα', value: 'watering' },
+    { label: 'Συγκομιδή', value: 'harvest' },
+    { label: 'Ψέκασμα', value: 'spraying' },
   ]);
+
+  const [date, setDate] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const toggleDatePicker = () => setShowDatePicker(!showDatePicker);
+  const onChangeDate = (event: any, selectedDate: any) => {
+    if(event.type === 'dismissed') {
+      toggleDatePicker();
+      return;
+    };
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+    toggleDatePicker();
+  }
 
   useEffect(() => {
     navigation.setOptions({ title: 'Προσθήκη Εργασίας' });
@@ -39,15 +51,22 @@ export default function FormScreen() {
 
   const validateForm = () => {
     if (!taskType) return false;
+    if(taskType === 'grinding') {
+      if (!oliveQuantity || !oilQuantity || !oxide) return false;
+    } else if (taskType === 'fertilization' || taskType === 'spraying') {
+      if (!cost || !name) return false;
+    } else if (taskType === 'watering') {
+      if (!indication) return false;
+    } else if (taskType === 'harvest') {
+      if (!cost || !sacks) return false
+    }
     return true;
   };
 
   const handleSubmit = () => {
     if (!validateForm()) Alert.alert('Προσοχη!', 'Συμπληρώστε τα υποχρεωτικά πεδία');
     else {
-    //   addField(name, location, parseInt(totalTrees), parseFloat(size), description);
-    //   Alert.alert('Επιτυχής Προσθήκη', 'Το χωράφι προστέθηκε επιτυχώς');
-    //   router.back();
+      Alert.alert("ok");
     }
   };
 
@@ -68,95 +87,115 @@ export default function FormScreen() {
         arrowIconStyle = {{tintColor: 'white'}}
         tickIconStyle = {{tintColor: 'white'}}
       />
-
-      { taskType === 'Άλεση' && (<>
-        <Text style={styles.label}>Ποσότητα Άλεσης (κιλά) *</Text>
-        <TextInput 
-          style={styles.input} 
-          keyboardType="numeric"
-          value={oliveQuantity}
-          onChangeText={text => setOliveQuantity(parseInt(text))}
-        />
-
-        <Text style={styles.label}>Ποσότητα Ελαιολάδου (κιλά) *</Text>
-        <TextInput 
-          style={styles.input} 
-          keyboardType="numeric"
-          value={oilQuantity}
-          onChangeText={text => setOilQuantity(parseInt(text))}
-        />
-
-        <Text style={styles.label}>Οξύτητα (%) *</Text>
-        <TextInput 
-          style={styles.input} 
-          keyboardType="numeric"
-          value={oxide}
-          onChangeText={text => setOxide(parseInt(text))}
-        />
-      </>)}
-
-      { (taskType === 'Λίπασμα' || taskType === 'Ψέκασμα') && (<>
-        <Text style={styles.label}>Κόστος (€) *</Text>
-        <TextInput 
-          style={styles.input} 
-          keyboardType="numeric"
-          value={cost}
-          onChangeText={text => setCost(parseInt(text))}
-        />
-
-        <Text style={styles.label}>Όνομα Φαρμάκου *</Text>
-        <TextInput 
-          style={styles.input} 
-          value={name}
-          onChangeText={text => setName(text)}
-        />
-      </>)}
-
-      { taskType === 'Πότισμα' && (<>
-        <Text style={styles.label}>Ένδειξη μετρητή *</Text>
-        <TextInput 
-          style={styles.input} 
-          keyboardType="numeric"
-          value={indication}
-          onChangeText={text => setIndication(parseInt(text))}
-        />
-        <Text style={styles.label}>Το κόστος του ποτίσματος υπολογίζεται με βάση την τιμη ανα κυβικό που έχει καταχωρηθεί για το εκάστοτε χωράφι. </Text>
-      </>)}
-
-      { taskType === 'Συγκομιδή' && (<>
-        <Text style={styles.label}>Κόστος (€) *</Text>
-        <TextInput 
-          style={styles.input} 
-          keyboardType="numeric"
-          value={cost}
-          onChangeText={text => setCost(parseInt(text))}
-        />
-
-        <Text style={styles.label}>Συνολικά Τσουβάλια *</Text>
-        <TextInput 
-          style={styles.input} 
-          keyboardType="numeric"
-          value={sacks}
-          onChangeText={text => setSacks(parseInt(text))}
-        />
-      </>)}
-
-      <Text style={styles.label}>Σχόλια</Text>
-      <TextInput
-        style={styles.input}
-        value={description}
-        onChangeText={setDescription}
-        multiline
-       />
-    
-      <View style={styles.buttons}>
-        <Pressable style={styles.submit} onPress={handleSubmit}>
-            <Text style={styles.text}>Προσθήκη</Text>
+      <ScrollView>
+        <Text style={styles.label}>Ημερομηνία *</Text>
+        {
+          showDatePicker && (
+            <DateTimePicker
+              mode='date'
+              display='spinner'
+              value={date}
+              onChange={onChangeDate}
+            />
+          )
+        }
+        <Pressable onPress={toggleDatePicker}>
+          <TextInput style={styles.input}
+            value={date.toLocaleDateString('el-GR')}
+            editable={false}
+            onChangeText={text => setDate(new Date(text))}
+          />
         </Pressable>
-        <Pressable style={styles.cancel} onPress={() => router.back()}>
-            <Text style={styles.underlineText}>Ακύρωση</Text>
-        </Pressable>
-      </View>
+  
+        { taskType === 'grinding' && (<>
+          <Text style={styles.label}>Ποσότητα Άλεσης (κιλά) *</Text>
+          <TextInput 
+            style={styles.input} 
+            keyboardType="numeric"
+            value={oliveQuantity}
+            onChangeText={text => setOliveQuantity(parseInt(text))}
+          />
+
+          <Text style={styles.label}>Ποσότητα Ελαιολάδου (κιλά) *</Text>
+          <TextInput 
+            style={styles.input} 
+            keyboardType="numeric"
+            value={oilQuantity}
+            onChangeText={text => setOilQuantity(parseInt(text))}
+          />
+
+          <Text style={styles.label}>Οξύτητα (%) *</Text>
+          <TextInput 
+            style={styles.input} 
+            keyboardType="numeric"
+            value={oxide}
+            onChangeText={text => setOxide(parseFloat(text))}
+          />
+        </>)}
+
+        { (taskType === 'fertilization' || taskType === 'spraying') && (<>
+          <Text style={styles.label}>Κόστος (€) *</Text>
+          <TextInput 
+            style={styles.input} 
+            keyboardType="numeric"
+            value={cost}
+            onChangeText={text => setCost(parseInt(text))}
+          />
+
+          <Text style={styles.label}>Όνομα Φαρμάκου *</Text>
+          <TextInput 
+            style={styles.input} 
+            value={name}
+            onChangeText={text => setName(text)}
+          />
+        </>)}
+
+        { taskType === 'watering' && (<>
+          <Text style={styles.label}>Ένδειξη μετρητή *</Text>
+          <TextInput 
+            style={styles.input} 
+            keyboardType="numeric"
+            value={indication}
+            onChangeText={text => setIndication(parseInt(text))}
+          />
+          <Text style={styles.label}>Το κόστος του ποτίσματος υπολογίζεται με βάση την τιμη ανα κυβικό που έχει καταχωρηθεί για το εκάστοτε χωράφι. </Text>
+        </>)}
+
+        { taskType === 'harvest' && (<>
+          <Text style={styles.label}>Κόστος (€) *</Text>
+          <TextInput 
+            style={styles.input} 
+            keyboardType="numeric"
+            value={cost}
+            onChangeText={text => setCost(parseInt(text))}
+          />
+
+          <Text style={styles.label}>Συνολικά Τσουβάλια *</Text>
+          <TextInput 
+            style={styles.input} 
+            keyboardType="numeric"
+            value={sacks}
+            onChangeText={text => setSacks(parseInt(text))}
+          />
+        </>)}
+
+        <Text style={styles.label}>Σχόλια</Text>
+        <TextInput
+          style={styles.input}
+          value={description}
+          onChangeText={setDescription}
+          multiline
+        />
+      
+        <View style={styles.buttons}>
+          <Pressable style={styles.submit} onPress={handleSubmit}>
+              <Text style={styles.text}>Προσθήκη</Text>
+          </Pressable>
+          <Pressable style={styles.cancel} onPress={() => router.back()}>
+              <Text style={styles.underlineText}>Ακύρωση</Text>
+          </Pressable>
+        </View>
+      </ScrollView>
     </View>
   );
 }
